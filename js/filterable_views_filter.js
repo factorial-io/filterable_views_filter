0f6d88cf2caf14d8751a7fce0bb16efede22c8dc
muschpusch <volkan@rhizom.nl>
2015-04-26 12:03:33 +0200

    Initial commit

---
+++ js/filterable_views_filter.js
@@ -0,0 +1,58 @@
+/**
+ * @file
+ * Handles Views' exposed form AJAX data submission.
+ */
+
+
+(function($) {
+
+  /**
+   * Loads content from url and attaches behaviours properly.
+   * @see filterable_views_filter_ajax_callback
+   */
+  $.fn.pROJECTLoad = function(url, selector) {
+    jQuery(selector).load(url, function() {
+      var content = jQuery(selector);
+      Drupal.attachBehaviors(content);
+    });
+  };
+
+  Drupal.behaviors.filterable_views_filter = {
+    attach: function() {
+      $('.state-selector').change(function(e){
+        var formSelector = '.' + this.value;
+        $(formSelector).removeClass('element-hidden').addClass('element-not-hidden');
+      });
+      $('.state-close').click(function(e){
+        var filter = $(e.target).parents('[class^="filter-"]');
+        var classes = filter.attr("class").split(' ');
+
+        $.each(Drupal.settings.filterable_views_filter, function(index, value){
+          if ($.inArray(index, classes) != -1){
+            $(filter).find(':input').val(value);
+          }
+        });
+        var id = $(this).parents('.element-not-hidden').attr('id');
+        $(this).parents('.element-not-hidden').addClass('element-hidden')
+      });
+    }
+  };
+
+  /**
+   * Gets Form build info from settings and adds it to ajax data.
+   *
+   * @see views_exposed_form_ajax_enable().
+   */
+  Drupal.behaviors.PROJECTViewsExposedFormAjax = {
+    attach: function(context, settings) {
+      for (var ajaxObject in Drupal.ajax) {
+        for(var name in Drupal.settings.ViewsExposedFormInfo) {
+          if (Drupal.ajax[ajaxObject].options && Drupal.ajax[ajaxObject].options.data._triggering_element_name == name) {
+            jQuery.extend(Drupal.ajax[ajaxObject].options.data, Drupal.settings.ViewsExposedFormInfo[name]);
+          }
+        }
+      }
+    }
+  };
+
+})(jQuery);
